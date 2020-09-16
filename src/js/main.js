4ad10ee3321437c6607ae85dfee9fb2a852cf863
DESKTOP-7UQJTRQ\Quy_cut3 <phamhoangquy99vn@gmail.com>
2020-09-16 16:25:37 +0700

    Update service & detail

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,141 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$('.menu-mobile-toggle').click(function(){
-		$('.offcanvas-wrap').fadeIn().addClass('offcanvas-open')
-	})
+    $('.menu-mobile-toggle').click(function() {
+        $('.offcanvas-wrap').fadeIn().addClass('offcanvas-open')
+    })
 
-	$('.offcanvas-wrap').click(function(e){
-		if(e.target !== e.currentTarget) return
-		$('.offcanvas-wrap').fadeOut().removeClass('offcanvas-open')
-	})
-
+    $('.offcanvas-wrap').click(function(e) {
+        if (e.target !== e.currentTarget) return
+        $('.offcanvas-wrap').fadeOut().removeClass('offcanvas-open')
+    })
+    swiperInit();
 });
 
 const mainMenuMobileMapping = new MappingListener({
-  selector: ".menu-wrapper",
-  mobileWrapper: ".mobile-menu",
-  mobileMethod: "appendTo",
-  desktopWrapper: ".right-header",
-  desktopMethod: "appendTo",
-  breakpoint: 1024.98,
+    selector: ".menu-wrapper",
+    mobileWrapper: ".mobile-menu",
+    mobileMethod: "appendTo",
+    desktopWrapper: ".right-header",
+    desktopMethod: "appendTo",
+    breakpoint: 1024.98,
 }).watch();
 
 const webNavMobileMapping = new MappingListener({
-  selector: ".website-nav-wrap",
-  mobileWrapper: ".mobile-menu",
-  mobileMethod: "appendTo",
-  desktopWrapper: ".right-top-header",
-  desktopMethod: "prependTo",
-  breakpoint: 1024.98,
+    selector: ".website-nav-wrap",
+    mobileWrapper: ".mobile-menu",
+    mobileMethod: "appendTo",
+    desktopWrapper: ".right-top-header",
+    desktopMethod: "prependTo",
+    breakpoint: 1024.98,
 }).watch();
+
+function swiperInit() {
+    var brandSwiper = new Swiper(".block_service-detail-3 .swiper-container", {
+        // Optional parameters
+        speed: 1000,
+        spaceBetween: 30,
+        autoplay: {
+            delay: 2000,
+        },
+        navigation: {
+            nextEl: ".block_service-detail-3 .nav-arrow-next",
+            prevEl: ".block_service-detail-3 .nav-arrow-prev",
+        },
+        breakpointsInverse: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            400: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1025: {
+                slidesPerView: 3,
+            },
+            1440: {
+                slidesPerView: 3,
+            },
+            1600: {
+                slidesPerView: 3,
+            },
+        },
+    });
+    var brandSwiper = new Swiper(".block_service-detail-4 .swiper-container", {
+        // Optional parameters
+        speed: 1000,
+        spaceBetween: 30,
+        autoplay: {
+            delay: 2000,
+        },
+        navigation: {
+            nextEl: ".block_service-detail-4 .nav-arrow-next",
+            prevEl: ".block_service-detail-4 .nav-arrow-prev",
+        },
+        breakpointsInverse: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            400: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1025: {
+                slidesPerView: 3,
+            },
+            1440: {
+                slidesPerView: 3,
+            },
+            1600: {
+                slidesPerView: 3,
+            },
+        },
+    });
+    var brandSwiper = new Swiper(".block_service-detail-5 .swiper-container", {
+        // Optional parameters
+        speed: 1000,
+        spaceBetween: 30,
+        autoplay: {
+            delay: 2000,
+        },
+        navigation: {
+            nextEl: ".block_service-detail-5 .nav-arrow-next",
+            prevEl: ".block_service-detail-5 .nav-arrow-prev",
+        },
+        breakpointsInverse: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            400: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1025: {
+                slidesPerView: 3,
+            },
+            1440: {
+                slidesPerView: 3,
+            },
+            1600: {
+                slidesPerView: 3,
+            },
+        },
+    });
+}
